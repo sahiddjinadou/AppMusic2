4666e54888b9e478d99bf6e0d6ddcdba7ca2fd8d
sahiddjinadou <djinadousahid@gmail.com>
2024-06-18 12:10:00 +0200

    envoie de l'album + afficharge du composant

--- src/components/AlbumComponent.vue
+++ src/components/AlbumComponent.vue
@@ -1,95 +1,147 @@
 <template>
   <section class="section">
-      <article v-for="(album, index) in albums" :key="index" class="article-container">
-        <h2 class="title">{{ album.genre }}</h2>
-        <div class="flex-col">
-          <RouterLink to="#" class="router-class">{{ album.title }}</RouterLink>
-          <p class="text">
-            {{ album.description }}
-          </p>
-        </div>
-        <div>
-          <ul class="flex-row">
-            <li v-for="( tag, index) in album.tags" :key="index" class="ul-items" >{{ tag }}</li>
-          </ul>
-        </div>
+    <article v-for="(album, index) in albums" :key="index" class="article-container">
+      <h2 class="title">{{ album.genre }}</h2>
+      <div class="flex-col">
+        <RouterLink to="#" class="router-class">{{ album.title }}</RouterLink>
+        <p class="text">
+          {{ album.description }}
+        </p>
+      </div>
+      <div>
+        <ul class="flex-row">
+          <li v-for="( tag, index) in album.tags" :key="index" class="ul-items">{{ tag }}</li>
+        </ul>
+      </div>
   
-        <div>
-          <ul>
-            <li>en</li>
-            <li>5</li>
-            <li>840min</li>
-            <li>Details</li>
-          </ul>
-        </div>
-      </article>
-
-    </section>
+      <div>
+        <ul class="flex-row ">
+          <li class="details border">
+            <span> en </span>
+          </li>
+          <li class="flex-row details ">
+           <img class="image" src="../assets/img/favorite.png" alt="favorite">
+           <span> 5 </span>
+          </li>
+          <li class="flex-row details  ">
+           <img class="image" src="../assets/img/timerPlay.png" alt="favorite">           
+           <span> {{ album.duration }} min</span>
+          </li>
+          <li  >
+            <button class="details border btn" @click="showDetails(album)"> Details </button>
+          </li>
+        </ul>
+      </div>
+    </article>
+  
+  </section>
 </template>
 
 <script setup>
 import albums from '@/assets/data';
+import { ref } from 'vue';
 
 const props = defineProps({
-    albums : Array
+  albums: Array
 })
+
+const emit = defineEmits(["albumData","upState"])
+const detailsState = ref(false)
+
+const showDetails = (element)=>{
+  emit("albumData",element) ; 
+  detailsState.value = true ; 
+  emit("upState",detailsState.value)
+}
+
+
 </script>
 
 <style scoped>
-.title{
+.btn {
+  font-family: "Playwrite România";
+  background-color: #fff;
+  display: block;
+  width: 100%;
+  height: 100%;
+}
+.btn:hover{
+  background-color: rgb(177, 177, 177);
+  transition: .5s;
+}
+.details {
+  padding-block:.3rem;
+  padding-inline: .5rem;
+}
+.border{
+  border:1px solid rgb(204, 196, 196);
+  border-radius:5px
+}
+.image{
+  display: block;
+  width: 25px;
+  height: 25px;
+}
+.title {
   border-radius: 10px 10px 0px 0px;
   background-color: aquamarine;
-  padding:.5rem ;
+  padding: .5rem;
   font-family: "Ubuntu";
 }
+
 .flex-col {
   display: flex;
   flex-direction: column;
   gap: .5rem;
 }
+
 .section {
   padding: 1rem;
 }
 
 .article-container {
   display: flex;
-  flex-direction: column; 
-  border: 1px solid ;
-  border-radius:10px;
-  margin-block:15px ;
+  flex-direction: column;
+  border: 1px solid;
+  border-radius: 10px;
+  margin-block: 15px;
 }
 
 
 .flex-row {
   display: flex;
   flex-direction: row;
+  flex-wrap: wrap;
 }
 
 .ul-items {
   padding-block: .5rem;
-  padding-inline: 1rem;
-  background-color:rgb(12, 126, 88);
+  padding-inline: .8rem;
+  background-color: rgb(12, 126, 88);
   color: white;
   font-family: "Ubuntu";
 }
+
 li {
   list-style: none;
   margin: .3rem;
 }
+
 .text {
-  padding: .5rem;
-    
+  padding: .3rem;
+
 }
+
 .router-class {
   color: black;
-  
+
   padding: .5rem;
   font-size: 18px;
   font-weight: bold;
   background-color: white;
 }
-.router-class:hover{
+
+.router-class:hover {
   text-decoration: underline;
 }
-
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,25 +2,40 @@
 import { ref, onMounted } from 'vue';
 import data from '@/assets/data';
 import AlbumComponent from '@/components/AlbumComponent.vue';
+import ContenuAlbumComponent  from "@/components/ContenuAlbumComponent.vue";
 
 //Notre tableau contenant les albums
 const albums = data;
 
-console.log(data);
+const showDetails = ref(false)
 
+const upShowDetails = (event)=>{
+  showDetails.value = event
+}
+
+const oneAlbum = ref()
+const upAlbum = (event)=>{
+  oneAlbum.value = event
+}
 </script>
 
 <template>
   <main class="main-container">
-    <section>
-      <form action="#">
-        <input type="search" name="" id="">
-        <input type="submit" value="Rechercher">
-      </form>
-      <h1>Page principale Album Music</h1>
-    </section>
-
-    <AlbumComponent :albums="albums"/>
+    <div class="gille">
+      <!-- Details -->
+      <ContenuAlbumComponent  v-if="showDetails" :album="oneAlbum"/>
+      <!-- contenues -->
+      <div>
+        <section>
+          <form action="#">
+            <input type="search" name="" id="">
+            <input type="submit" value="Rechercher">
+          </form>
+          <h1>Page principale Album Music</h1>
+        </section>
+        <AlbumComponent :albums="albums" @upState = 'upShowDetails' @albumData = 'upAlbum'/>
+      </div>
+    </div>
     
   </main>
 </template>
@@ -32,6 +47,9 @@ console.log(data);
   text-align: center;
   margin-block:1rem;
 }
-
+.grille {
+  display:grid;
+  grid-auto-columns: 1fr;
+}
 
 </style>
