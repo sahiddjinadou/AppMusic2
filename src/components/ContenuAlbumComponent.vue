646868b7da80b601f87ffd53e326e3aca93dd470
sahiddjinadou <djinadousahid@gmail.com>
2024-06-18 18:44:00 +0200

    finafisation du aside

--- src/components/ContenuAlbumComponent.vue
+++ src/components/ContenuAlbumComponent.vue
@@ -1,21 +1,21 @@
 <template>
-  <aside>
+  <aside class="aside1">
     <div>
-      <div>
+      <div class="metadata">
         <h2 class="title">{{ album?.title }}</h2>
         <p> <strong>{{ album?.artist }}</strong></p>
         <button @click="sendProgress" class="btn-play">
           <p> play</p>
-          <p> <img src="../assets/img/play_btn.png" alt="play"></p>          
+          <p class="poster"> <img src="../assets/img/play_btn.png" alt="play"></p>          
         </button>
       </div>
       <img :src="album?.coverImage" alt="image">
     </div>
   
     <div class="list">
-      <h3> List of song </h3>
+      <h3> List of songs </h3>
       <ul>
-        <li v-for="track in album?.tracks" :key="track.trackNumber" class="flex">
+        <li v-for="track in album?.tracks" :key="track.trackNumber" class="flex song">
           <p>{{ track.title }}</p>
           <p>{{ track.duration }}</p>
         </li>
@@ -42,22 +42,90 @@ const sendProgress = () => {
 </script>
 
 <style scoped>
+
+.poster {
+  width: 30px;
+  height: 30px;
+}
+[alt="image"] {
+  width: 100%;
+  height: 100%;
+}
+.list h3 {
+  background-color: #3333ee;
+  padding: 1rem;
+  color: white;
+  font-family: "Ubuntu";
+  font-size: 20px;
+}
+.song p:first-child {
+  font-family: "Playwrite România";
+  font-weight: bold;
+}
+.aside1 {
+  padding-block: 1rem;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  border-radius:10px ;
+  height: 60%;
+  width: 750px;
+  box-shadow: 5px 5px 10px;
+}
+.song:hover{
+  background-color: beige;
+  
+}
+.song {
+  padding: .5rem 1rem;
+}
+.aside1 >:first-child {
+  display: flex;
+  flex-direction: column-reverse;
+  justify-content: center;
+  align-items: center;
+}
+.metadata {
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  
+}
 .flex {
   display: flex;
   justify-content: space-between;
 }
 
 .btn-play{
-background-color: red;
+background-color: rgba(70, 122, 233, 0.856);
 border: none;
 display: flex;
-}
-.btn-play >:first-child,
-.btn-play >:last-child
-{
-  border:1px solid
+justify-content: center;
+align-items: center;
+gap: 10px;
+padding-inline: 1rem;
+padding-block: .4rem;
+border-radius: 10px;
 
 }
+.btn-play:hover{
+  background-color: rgba(0, 0, 255, 0.884);
+}
+
+.btn-play> :first-child{
+  font-family: "Ubuntu";
+  color:white;
+  font-size: 15px;
+  text-transform: capitalize;
+}
+[alt= "play"] {
+  border: 1px solid;
+  width:30px;
+  height:30px;
+  border-radius: 50%;
+  border: 2px solid white;
+}
+
 strong{
   font-family: "Playwrite România";
   font-size: 21px;
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -21,15 +21,15 @@ const upAlbum = (event)=>{
 
 <template>
   <main class="main-container">
-    <div class="gille">
+    <div class="grille">
       <!-- Details -->
       <ContenuAlbumComponent  v-if="showDetails" :album="oneAlbum"/>
       <!-- contenues -->
       <div>
         <section>
-          <form action="#">
+          <form class="form" >
             <input type="search" name="" id="">
-            <input type="submit" value="Rechercher">
+            <input type="submit"  class="button-83" value="Rechercher">
           </form>
           <h1>Page principale Album Music</h1>
         </section>
@@ -43,13 +43,71 @@ const upAlbum = (event)=>{
 
 
 <style scoped>
+.form {
+  display:  flex;
+  justify-content: center;
+  gap: 20px;
+  margin-block: 2rem;
+}
 .main-container section:first-of-type {
   text-align: center;
   margin-block:1rem;
 }
 .grille {
   display:grid;
-  grid-auto-columns: 1fr;
+}
+[type="search"]{
+  padding: 1rem;
+  font-size: 20px;
 }
 
+
+/* CSS */
+.button-83 {
+  appearance: button;
+  background-color: transparent;
+  background-image: linear-gradient(to bottom, #fff, #f8eedb);
+  border: 0 solid #e5e7eb;
+  border-radius: .5rem;
+  box-sizing: border-box;
+  color: #482307;
+  column-gap: 1rem;
+  cursor: pointer;
+  /* display: flex; */
+  font-family: ui-sans-serif,system-ui,-apple-system,system-ui,"Segoe UI",Roboto,"Helvetica Neue",Arial,"Noto Sans",sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol","Noto Color Emoji";
+  font-size: 100%;
+  font-weight: 700;
+  line-height: 24px;
+  margin: 0;
+  outline: 2px solid transparent;
+  padding: 1rem 1.5rem;
+  text-align: center;
+  text-transform: none;
+  transition: all .1s cubic-bezier(.4, 0, .2, 1);
+  user-select: none;
+  -webkit-user-select: none;
+  touch-action: manipulation;
+  box-shadow: -6px 8px 10px rgba(81,41,10,0.1),0px 2px 2px rgba(81,41,10,0.2);
+}
+
+.button-83:active {
+  background-color: #f3f4f6;
+  box-shadow: -1px 2px 5px rgba(81,41,10,0.15),0px 1px 1px rgba(81,41,10,0.15);
+  transform: translateY(0.125rem);
+}
+
+.button-83:focus {
+  box-shadow: rgba(72, 35, 7, .46) 0 0 0 4px, -6px 8px 10px rgba(81,41,10,0.1), 0px 2px 2px rgba(81,41,10,0.2);
+}
+@media (min-width: 600px) {
+  .grille{
+    display: flex;
+    justify-content: space-between;
+    gap: 100px;
+  }
+  /* .grille{
+    grid-template-columns: 20% 75%;
+    gap: 50px;
+  } */
+}
 </style>
